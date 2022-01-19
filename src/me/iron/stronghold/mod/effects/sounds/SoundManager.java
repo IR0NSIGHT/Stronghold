3b282e54cf5187e520325203b4071461514db81f
irnsght <irnsght@gmail.com>
2022-01-19 19:09:04 +0100

    deactivate debug that plays all sounds

--- src/me/iron/stronghold/mod/effects/sounds/SoundManager.java
+++ src/me/iron/stronghold/mod/effects/sounds/SoundManager.java
@@ -34,7 +34,7 @@ public class SoundManager {
             System.out.println("HOORAY!");
         }
         initSounds();
-        initDebug();
+    //    initDebug();
         initLoop();
     }
 
