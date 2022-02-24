c8376a5b290575f39a6eac3e310872d9e8abc4f1
irnsght <irnsght@gmail.com>
2022-02-24 14:12:55 +0100

    BLUE-ON-BLUE allowed in PVP area, sends warning on violation, logs violation

--- src/me/iron/stronghold/mod/implementation/PveShield.java
+++ src/me/iron/stronghold/mod/implementation/PveShield.java
@@ -1,5 +1,8 @@
 package me.iron.stronghold.mod.implementation;
 
+import api.DebugFile;
+import api.utils.game.PlayerUtils;
+import me.iron.stronghold.mod.ModMain;
 import org.schema.common.util.linAlg.Vector3i;
 import org.schema.game.common.controller.damage.DamageDealerType;
 import org.schema.game.common.controller.damage.Damager;
@@ -7,6 +10,8 @@ import org.schema.game.common.controller.damage.HitType;
 import org.schema.game.common.controller.damage.effects.InterEffectSet;
 import org.schema.game.common.controller.elements.ShieldAddOn;
 import org.schema.game.common.data.world.SectorNotFoundException;
+import org.schema.schine.common.language.Lng;
+import org.schema.schine.network.server.ServerMessage;
 
 import javax.vecmath.Vector3f;
 
@@ -19,10 +24,14 @@ public class PveShield extends VoidShield {
     public double handleShieldHit(ShieldAddOn shieldAddOn, Damager damager, InterEffectSet defenseSet, Vector3f hitPoint, int projectileSectorId, DamageDealerType damageType, HitType hitType, double damage, long weaponId) throws SectorNotFoundException {
         assert getParent() != null;
         if (isActive() &&
-            ((StellarControllableArea)getParent()).isSectorInArea(shieldAddOn.getSegmentController().getSector(new Vector3i())) &&
+            ((StellarControllableArea)getParent()).isSectorInArea(shieldAddOn.getSegmentController().getSector(new Vector3i())) && //inside geometry of parent area
+                (shieldAddOn.getSegmentController().getFactionId()!=damager.getFactionId()) && //not blue-on-blue
                 (shieldAddOn.getSegmentController().getFactionId()>0&&damager.getFactionId()>0) //hit is player AND damager is player
         ) //allows damage from NPCs or to NPCs
+        {
+            onPeaceViolated(damager);
             return 0;
+        }
         return damage;
     }
 
@@ -31,6 +40,11 @@ public class PveShield extends VoidShield {
         //to off? no
         //to on? yes
         return to;
+    }
+
+    protected void onPeaceViolated(Damager d) {
+        d.sendServerMessage(Lng.astr("This is a PVE area. No PVP allowed."), ServerMessage.MESSAGE_TYPE_WARNING);
+        DebugFile.log("[PVE VIOLATION]"+d.getShootingEntity().getUniqueIdentifier()+"["+d.getShootingEntity().getFaction().getName()+"] violated PVE rules ", ModMain.instance);
     }
 
     @Override
